dbafdf2fcde5b8119df6cf5eba4a354645d16c3c
guiem <g@guiem.info>
2018-03-15 18:11:37 +0000

    New fields to display in admin admin's Metaphor view

--- metaphor/admin.py
+++ metaphor/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import Metaphor, Dictionary
 
 class SentenceAdmin(admin.ModelAdmin):
-    list_display = ('sentence_text', 'metaphor_text', 'req_date', 'remote_addr')
+    list_display = ('sentence_text', 'metaphor_text', 'req_date', 'remote_addr', 'strategy', 'upvotes', 'downvotes',
+                    'total_votes')
 
 admin.site.register(Metaphor, SentenceAdmin)
 
