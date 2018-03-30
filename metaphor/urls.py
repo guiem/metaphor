1f9228f1bbbe59e64c79204e9924b433c9fe4ab7
guiem <g@guiem.info>
2018-03-30 18:08:20 +0100

    Autoload Embeddings at start to prevent server timeouts later

--- metaphor/urls.py
+++ metaphor/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from django.urls import path
-from . import views
 from django.contrib import admin
+from . import views
+from metaphor.ai.embeddings import Embeddings
+from metaphor.settings import BASE_DIR
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
@@ -10,3 +13,10 @@ urlpatterns = [
     path('strategies/', views.strategies, name='strategies'),
     path('metaphors/vote/', views.vote, name='vote'),
 ]
+
+# Operations that will be executed at the beginning to avoid timeouts
+
+# 1. Loading embeddings at start
+emb_path = os.path.join(BASE_DIR, 'data/glove.6B/glove.6B.50d.txt')
+emb_info = {'glove.6B.50d': {'path': emb_path, 'dim':50, 'sim_index': True}}
+e = Embeddings('Embeddings', emb=emb_info)
